73d4128ba5f9e5ce17d07eff9b567355d7863894
Mohammad Bidokhti <123873128+Petrosbid@users.noreply.github.com>
2025-11-28 12:30:17 +0330

    update structure

--- plants/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Plant
-
-class PlantSerializer(serializers.ModelSerializer):
-    """Read-only serializer for the Plant model."""
-    class Meta:
-        model = Plant
-        fields = '__all__'
--- users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    # Add any additional fields you want to display in the admin
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
